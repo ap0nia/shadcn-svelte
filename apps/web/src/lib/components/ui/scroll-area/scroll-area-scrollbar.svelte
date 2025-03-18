07c97b955b014a476662fd7a8505bdba29ea20bf
Aponia <ap0nia.dev@gmail.com>
2025-03-18 11:04:37 -0700

    style(web): 🎨 bg-base for scroll-area thumb background

--- apps/web/src/lib/components/ui/scroll-area/scroll-area-scrollbar.svelte
+++ apps/web/src/lib/components/ui/scroll-area/scroll-area-scrollbar.svelte
@@ -24,6 +24,6 @@
 >
   {@render children?.()}
   <ScrollAreaPrimitive.Thumb
-    class={cn('bg-border relative rounded-full', orientation === 'vertical' && 'flex-1')}
+    class={cn('bg-base-300 relative rounded-full', orientation === 'vertical' && 'flex-1')}
   />
 </ScrollAreaPrimitive.Scrollbar>
