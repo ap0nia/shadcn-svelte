c1d0cca62314bbad34c131ad2c153a6c97845e47
Aponia <ap0nia.dev@gmail.com>
2025-03-18 11:48:19 -0700

    feat(web): ✨ language selector

---
+++ apps/web/src/lib/components/language-select.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import * as Select from '$lib/components/ui/select'
+  import * as messages from '$lib/paraglide/messages'
+  import { getLocale, locales, setLocale, type Locale } from '$lib/paraglide/runtime'
+  import { cn } from '$lib/utils/cn'
+
+  let { class: className = '' } = $props()
+
+  const locale = getLocale()
+
+  async function handleSelectedChange(selected?: string) {
+    if (selected == null) return
+    setLocale(selected as Locale, { reload: false })
+  }
+</script>
+
+<!--
+@component
+
+A select menu that navigates to different language settings.
+-->
+
+<div data-tip={messages.selectLanguage()} class="tooltip tooltip-bottom">
+  <Select.Root type="single" value={locale} onValueChange={handleSelectedChange}>
+    <Select.Trigger class={cn(className, 'min-w-28')}>
+      {messages.__name() || messages.selectLanguage()}
+    </Select.Trigger>
+
+    <Select.Content class="!w-auto">
+      <Select.Group class="menu w-full p-0">
+        {#each locales as locale}
+          <Select.Item value={locale}>
+            {messages.__code({}, { locale })}
+            {messages.__name({}, { locale })}
+          </Select.Item>
+        {/each}
+      </Select.Group>
+    </Select.Content>
+  </Select.Root>
+</div>
