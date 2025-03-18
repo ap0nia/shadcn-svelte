b04241fb9b407ba55c801471aacf7b08fb97b4d1
Aponia <ap0nia.dev@gmail.com>
2025-03-18 12:48:23 -0700

    feat(web): ✨ theme toggle button component

---
+++ apps/web/src/lib/components/theme-toggle.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { mode, setTheme, toggleMode } from 'mode-watcher'
+
+  import { messages } from '$lib/i18n'
+  import { cn } from '$lib/utils/cn'
+
+  function toggleTheme() {
+    toggleMode()
+
+    /**
+     * The current mode, with default being 'light'.
+     */
+    const currentMode = $mode ?? 'light'
+
+    /**
+     * See if a more specific theme can be found by using the current mode.
+     */
+    const theme = localStorage.getItem(currentMode) ?? currentMode
+
+    setTheme(theme)
+  }
+</script>
+
+<!--
+@component
+
+Button that toggles between light and dark mode.
+It can also use specific themes specified by the 'light' and 'dark' keys from localstorage.
+-->
+
+<div data-tip={$messages.toggleTheme()} class="tooltip tooltip-bottom">
+  <button
+    onclick={toggleTheme}
+    class="btn btn-sm btn-square ring-base-content ring-1"
+    aria-label="Color scheme toggle"
+  >
+    <span class="swap swap-rotate" class:swap-active={$mode === 'dark'}>
+      <span
+        class={cn(
+          'icon-[mdi--moon-waxing-crescent] swap-on size-5',
+
+          // $mode is undefined on the server and thus on mount.
+          // Before it's mounted for the first time, force the dark icon to be static.
+          !$mode && 'dark:!rotate-0 dark:!opacity-100',
+        )}
+      ></span>
+      <span class="icon-[mdi--weather-sunny] swap-off size-5 dark:opacity-0"></span>
+    </span>
+  </button>
+</div>
