dd6ae69afa4a4e722ee2b044cd9f2568b19cbbe1
Aponia <ap0nia.dev@gmail.com>
2025-03-18 14:16:08 -0700

    chore(web): 🔧 remove i18n-watcher component
    - didn't work properly because it wasn't wrapping the children, so
      context was not set

--- apps/web/src/lib/components/i18n-watcher.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { createObservableLocale, setLocale, setMessages } from '$lib/i18n'
-
-  const locale = createObservableLocale()
-
-  setLocale(locale)
-
-  setMessages(locale)
-</script>
