59db825ebb1fafd7214f6e7dd5ef76008320dcf6
Aponia <ap0nia.dev@gmail.com>
2025-03-18 14:19:00 -0700

    feat(web): ✨ dynamically rendered docs page

---
+++ apps/web/src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { PageProps } from './$types'
+
+  let { data }: PageProps = $props()
+</script>
+
+<main>
+  {#if data.component}
+    <data.component />
+  {:else}
+    <h1>{data.slug}</h1>
+  {/if}
+</main>
---
+++ apps/web/src/routes/docs/[...slug]/+page.ts
@@ -0,0 +1,48 @@
+import type { EntryGenerator, PageLoad } from './$types'
+
+const modules = import.meta.glob('/src/lib/content/**/*.md')
+
+export const load: PageLoad = async (event) => {
+  const slug = event.params.slug
+
+  const markdownModuleKey = Object.keys(modules).find((key) => {
+    const moduleSlug = key.replace('/src/lib/content/docs/', '').replace('.md', '')
+    return moduleSlug == slug
+  })
+
+  console.log('LOAD', { slug, markdownModuleKey })
+
+  if (markdownModuleKey == null) {
+    return {
+      slug,
+    }
+  }
+
+  const importModule = modules[markdownModuleKey]
+
+  if (importModule == null) {
+    return {
+      slug,
+    }
+  }
+
+  const exports: any = await importModule()
+
+  const component = exports.default
+
+  return {
+    component,
+    slug,
+  }
+}
+
+export const entries: EntryGenerator = () => {
+  console.info('Prerendering /docs')
+
+  const entries = Object.keys(modules).map((path) => {
+    const slug = path.replace('/src/content/', '').replace('.md', '').replace('/index', '')
+    return { slug }
+  })
+
+  return entries
+}
