c2857f1eb76a13317f91cc083fb7deb2025bc4ab
Aponia <ap0nia.dev@gmail.com>
2025-03-18 23:38:16 -0700

    feat(web): ✨ tabs ui component

---
+++ apps/web/src/lib/components/ui/tabs/tabs-list.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Tabs as TabsPrimitive } from 'bits-ui'
+  import { cn } from '$lib/utils/cn.js'
+
+  let { ref = $bindable(null), class: className, ...restProps }: TabsPrimitive.ListProps = $props()
+</script>
+
+<TabsPrimitive.List
+  bind:ref
+  class={cn(
+    // 'bg-muted text-muted-foreground inline-flex h-9 items-center justify-center rounded-lg p-1',
+    'bg-base-200 text-base-content inline-flex h-10 items-center justify-center rounded-md',
+    className,
+  )}
+  {...restProps}
+/>
---
+++ apps/web/src/lib/components/ui/tabs/tabs-trigger.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { Tabs as TabsPrimitive } from 'bits-ui'
+  import { cn } from '$lib/utils/cn.js'
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    value,
+    ...restProps
+  }: TabsPrimitive.TriggerProps = $props()
+</script>
+
+<TabsPrimitive.Trigger
+  bind:ref
+  class={cn(
+    'btn no-animation',
+    'ring-offset-base-100 h-full min-h-0 rounded-sm shadow-none',
+    'focus-visible:ring-ring focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
+    'disabled:btn-disabled',
+    'data-[state=active]:btn-active',
+    className,
+  )}
+  {value}
+  {...restProps}
+/>
