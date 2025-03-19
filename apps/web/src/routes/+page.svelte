2b279fb6e871f6deed430a40dd7ebecb68f9daec
Aponia <ap0nia.dev@gmail.com>
2025-03-18 23:42:23 -0700

    feat(web): ✨ tab demo

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
+  import { toast } from 'svelte-sonner'
+
+  import * as Tabs from '$lib/components/ui/tabs'
   import { getMessages } from '$lib/i18n'
   import SvelteCounter from '$lib/markdown/svelte-counter.md'
   import ReactCounter from '$lib/markdown/react-counter.md'
-    import { toast } from 'svelte-sonner'
 
   const messages = getMessages()
 </script>
 
 <main class="space-y-4 p-4">
   <h1>{$messages.hello()}</h1>
+
+  <div class="flex justify-center">
+    <Tabs.Root value="account" class="min-w-md">
+      <Tabs.List class="grid w-full grid-cols-2">
+        <Tabs.Trigger value="account">Account</Tabs.Trigger>
+        <Tabs.Trigger value="password">Password</Tabs.Trigger>
+      </Tabs.List>
+
+      <Tabs.Content value="account">A</Tabs.Content>
+
+      <Tabs.Content value="password">B</Tabs.Content>
+    </Tabs.Root>
+  </div>
 
   <div class="flex justify-center gap-2">
     <button
