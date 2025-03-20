6a6e4f165022fcc283c546994e787ec5615c1d2c
Aponia <ap0nia.dev@gmail.com>
2025-03-20 14:23:27 -0700

    feat(web): ✨ installation docs

---
+++ apps/web/src/lib/components/docs/linked-card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { getLocale } from '$lib/i18n'
+  import { localizeHref } from '$lib/paraglide/runtime'
+  import { cn } from '$lib/utils/cn'
+
+  let { class: className, href, children, ...restProps } = $props()
+
+  const locale = getLocale()
+
+  const localizedHref = $derived(localizeHref(href, { locale: $locale }))
+</script>
+
+<a
+  href={localizedHref}
+  class={cn('btn link-hover h-auto flex-col items-center justify-center p-6 sm:p-10', className)}
+  {...restProps}
+>
+  {@render children?.()}
+</a>
