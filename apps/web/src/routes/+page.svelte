268baece28beb22d0b10aea8e6ffbf9ba6c54f3d
Aponia <ap0nia.dev@gmail.com>
2025-03-23 11:58:47 -0700

    feat: ✨ daisy ui default accordion

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,57 +1,106 @@
 <script lang="ts">
-  import * as ShadAccordion from '$lib/registry/new-york/ui/accordion'
-  import * as DaisyAccordion from '$lib/registry/daisy-new-york/ui/accordion'
+  import * as ShadDefaultAccordion from '$lib/registry/default/ui/accordion'
+  import * as ShadNewYorkAccordion from '$lib/registry/new-york/ui/accordion'
+  import * as DaisyDefaultAccordion from '$lib/registry/daisy-default/ui/accordion'
+  import * as DaisyNewYorkAccordion from '$lib/registry/daisy-new-york/ui/accordion'
 </script>
 
-<main class="mx-auto w-full max-w-2xl p-8">
+<main class="mx-auto w-full max-w-4xl p-8">
   <div class="theme-zinc">
     <h1>shadcn UI</h1>
 
-    <ShadAccordion.Root type="single" class="w-full sm:max-w-[70%]">
-      <ShadAccordion.Item value="item-1">
-        <ShadAccordion.Trigger>Is it accessible?</ShadAccordion.Trigger>
-        <ShadAccordion.Content
-          >Yes. It adheres to the WAI-ARIA design pattern.</ShadAccordion.Content
-        >
-      </ShadAccordion.Item>
-      <ShadAccordion.Item value="item-2">
-        <ShadAccordion.Trigger>Is it styled?</ShadAccordion.Trigger>
-        <ShadAccordion.Content>
-          Yes. It comes with default styles that matches the other components' aesthetic.
-        </ShadAccordion.Content>
-      </ShadAccordion.Item>
-      <ShadAccordion.Item value="item-3">
-        <ShadAccordion.Trigger>Is it animated?</ShadAccordion.Trigger>
-        <ShadAccordion.Content>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </ShadAccordion.Content>
-      </ShadAccordion.Item>
-    </ShadAccordion.Root>
+    <div class="flex items-center gap-8">
+      <ShadDefaultAccordion.Root type="single" class="w-full sm:max-w-[70%]">
+        <ShadDefaultAccordion.Item value="item-1">
+          <ShadDefaultAccordion.Trigger>Is it accessible?</ShadDefaultAccordion.Trigger>
+          <ShadDefaultAccordion.Content
+            >Yes. It adheres to the WAI-ARIA design pattern.</ShadDefaultAccordion.Content
+          >
+        </ShadDefaultAccordion.Item>
+        <ShadDefaultAccordion.Item value="item-2">
+          <ShadDefaultAccordion.Trigger>Is it styled?</ShadDefaultAccordion.Trigger>
+          <ShadDefaultAccordion.Content>
+            Yes. It comes with default styles that matches the other components' aesthetic.
+          </ShadDefaultAccordion.Content>
+        </ShadDefaultAccordion.Item>
+        <ShadDefaultAccordion.Item value="item-3">
+          <ShadDefaultAccordion.Trigger>Is it animated?</ShadDefaultAccordion.Trigger>
+          <ShadDefaultAccordion.Content>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </ShadDefaultAccordion.Content>
+        </ShadDefaultAccordion.Item>
+      </ShadDefaultAccordion.Root>
+
+      <ShadNewYorkAccordion.Root type="single" class="w-full sm:max-w-[70%]">
+        <ShadNewYorkAccordion.Item value="item-1">
+          <ShadNewYorkAccordion.Trigger>Is it accessible?</ShadNewYorkAccordion.Trigger>
+          <ShadNewYorkAccordion.Content
+            >Yes. It adheres to the WAI-ARIA design pattern.</ShadNewYorkAccordion.Content
+          >
+        </ShadNewYorkAccordion.Item>
+        <ShadNewYorkAccordion.Item value="item-2">
+          <ShadNewYorkAccordion.Trigger>Is it styled?</ShadNewYorkAccordion.Trigger>
+          <ShadNewYorkAccordion.Content>
+            Yes. It comes with default styles that matches the other components' aesthetic.
+          </ShadNewYorkAccordion.Content>
+        </ShadNewYorkAccordion.Item>
+        <ShadNewYorkAccordion.Item value="item-3">
+          <ShadNewYorkAccordion.Trigger>Is it animated?</ShadNewYorkAccordion.Trigger>
+          <ShadNewYorkAccordion.Content>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </ShadNewYorkAccordion.Content>
+        </ShadNewYorkAccordion.Item>
+      </ShadNewYorkAccordion.Root>
+    </div>
   </div>
 
   <div class="divider"></div>
 
   <div>
     <h1>daisyUI</h1>
-    <DaisyAccordion.Root type="single" class="w-full sm:max-w-[70%]">
-      <DaisyAccordion.Item value="item-1">
-        <DaisyAccordion.Trigger>Is it accessible?</DaisyAccordion.Trigger>
-        <DaisyAccordion.Content
-          >Yes. It adheres to the WAI-ARIA design pattern.</DaisyAccordion.Content
-        >
-      </DaisyAccordion.Item>
-      <DaisyAccordion.Item value="item-2">
-        <DaisyAccordion.Trigger>Is it styled?</DaisyAccordion.Trigger>
-        <DaisyAccordion.Content>
-          Yes. It comes with default styles that matches the other components' aesthetic.
-        </DaisyAccordion.Content>
-      </DaisyAccordion.Item>
-      <DaisyAccordion.Item value="item-3">
-        <DaisyAccordion.Trigger>Is it animated?</DaisyAccordion.Trigger>
-        <DaisyAccordion.Content>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </DaisyAccordion.Content>
-      </DaisyAccordion.Item>
-    </DaisyAccordion.Root>
+
+    <div class="flex items-center gap-8">
+      <DaisyDefaultAccordion.Root type="single" class="w-full sm:max-w-[70%]">
+        <DaisyDefaultAccordion.Item value="item-1">
+          <DaisyDefaultAccordion.Trigger>Is it accessible?</DaisyDefaultAccordion.Trigger>
+          <DaisyDefaultAccordion.Content
+            >Yes. It adheres to the WAI-ARIA design pattern.</DaisyDefaultAccordion.Content
+          >
+        </DaisyDefaultAccordion.Item>
+        <DaisyDefaultAccordion.Item value="item-2">
+          <DaisyDefaultAccordion.Trigger>Is it styled?</DaisyDefaultAccordion.Trigger>
+          <DaisyDefaultAccordion.Content>
+            Yes. It comes with default styles that matches the other components' aesthetic.
+          </DaisyDefaultAccordion.Content>
+        </DaisyDefaultAccordion.Item>
+        <DaisyDefaultAccordion.Item value="item-3">
+          <DaisyDefaultAccordion.Trigger>Is it animated?</DaisyDefaultAccordion.Trigger>
+          <DaisyDefaultAccordion.Content>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </DaisyDefaultAccordion.Content>
+        </DaisyDefaultAccordion.Item>
+      </DaisyDefaultAccordion.Root>
+
+      <DaisyNewYorkAccordion.Root type="single" class="w-full sm:max-w-[70%]">
+        <DaisyNewYorkAccordion.Item value="item-1">
+          <DaisyNewYorkAccordion.Trigger>Is it accessible?</DaisyNewYorkAccordion.Trigger>
+          <DaisyNewYorkAccordion.Content
+            >Yes. It adheres to the WAI-ARIA design pattern.</DaisyNewYorkAccordion.Content
+          >
+        </DaisyNewYorkAccordion.Item>
+        <DaisyNewYorkAccordion.Item value="item-2">
+          <DaisyNewYorkAccordion.Trigger>Is it styled?</DaisyNewYorkAccordion.Trigger>
+          <DaisyNewYorkAccordion.Content>
+            Yes. It comes with default styles that matches the other components' aesthetic.
+          </DaisyNewYorkAccordion.Content>
+        </DaisyNewYorkAccordion.Item>
+        <DaisyNewYorkAccordion.Item value="item-3">
+          <DaisyNewYorkAccordion.Trigger>Is it animated?</DaisyNewYorkAccordion.Trigger>
+          <DaisyNewYorkAccordion.Content>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </DaisyNewYorkAccordion.Content>
+        </DaisyNewYorkAccordion.Item>
+      </DaisyNewYorkAccordion.Root>
+    </div>
   </div>
 </main>
