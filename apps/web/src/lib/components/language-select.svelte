c1c0db6a1cc66866945befd6370a6cd9387c5fba
Aponia <ap0nia.dev@gmail.com>
2025-03-22 21:14:55 -0700

    chore(web): 🔧 remove tooltip from language select

--- apps/web/src/lib/components/language-select.svelte
+++ apps/web/src/lib/components/language-select.svelte
@@ -4,7 +4,7 @@
   import { locales, type Locale } from '$lib/paraglide/runtime'
   import { cn } from '$lib/utils/cn'
 
-  let { class: className = '', locale = getLocale(), messages = getMessages(locale) } = $props()
+  let { locale = getLocale(), messages = getMessages(locale) } = $props()
 
   async function handleSelectedChange(selected?: string) {
     if (selected == null) return
@@ -18,31 +18,29 @@
 A select menu that navigates to different language settings.
 -->
 
-<div data-tip={$messages.selectLanguage()} class={cn('tooltip tooltip-bottom', className)}>
-  <Select.Root type="single" value={$locale} onValueChange={handleSelectedChange}>
-    <Select.Trigger class={cn(false && 'w-28')}>
-      <!--
+<Select.Root type="single" value={$locale} onValueChange={handleSelectedChange}>
+  <Select.Trigger class={cn(false && 'w-28')}>
+    <!--
       <span class="hidden md:inline">
         {$messages.__name() || $messages.selectLanguage()}
       </span>
       -->
 
-      <span class={cn('icon-[mdi--language]', false && 'inline md:hidden')}></span>
-    </Select.Trigger>
+    <span class={cn('icon-[mdi--language]', false && 'inline md:hidden')}></span>
+  </Select.Trigger>
 
-    <Select.Content class="max-h-96" sideOffset={10}>
-      <Select.ScrollUpButton />
+  <Select.Content class="max-h-96" sideOffset={10}>
+    <Select.ScrollUpButton />
 
-      <Select.Group class="menu w-full p-0">
-        {#each locales as locale}
-          <Select.Item value={locale}>
-            {$messages.__code({}, { locale })}
-            {$messages.__name({}, { locale })}
-          </Select.Item>
-        {/each}
-      </Select.Group>
+    <Select.Group class="menu w-full p-0">
+      {#each locales as locale}
+        <Select.Item value={locale}>
+          {$messages.__code({}, { locale })}
+          {$messages.__name({}, { locale })}
+        </Select.Item>
+      {/each}
+    </Select.Group>
 
-      <Select.ScrollDownButton />
-    </Select.Content>
-  </Select.Root>
-</div>
+    <Select.ScrollDownButton />
+  </Select.Content>
+</Select.Root>
