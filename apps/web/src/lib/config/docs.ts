e4a177fd80676b69c5b5e1e72a1f4dd08299e7f1
Aponia <ap0nia.dev@gmail.com>
2025-03-23 09:35:38 -0700

    chore: 🔧 synchronize navigation header links

--- apps/web/src/lib/config/docs.ts
+++ apps/web/src/lib/config/docs.ts
@@ -13,106 +13,29 @@ export type NavItem = {
 
 export const main: NavItem[] = [
   {
-    title: 'Docs',
+    title: 'Documentation',
     href: '/docs',
   },
   {
     title: 'Components',
     href: '/docs/components/accordion',
-    items: [
-      // {
-      //   title: 'Alert Dialog',
-      //   href: '/docs/components/alert-dialog',
-      //   description:
-      //     'A modal dialog that interrupts the user with important content and expects a response.',
-      // },
-      // {
-      //   title: 'Link Preview',
-      //   href: '/docs/components/link-preview',
-      //   description: 'For sighted users to preview content available behind a link.',
-      // },
-      // {
-      //   title: 'Progress',
-      //   href: '/docs/components/progress',
-      //   description:
-      //     'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-      // },
-      // {
-      //   title: 'Scroll Area',
-      //   href: '/docs/components/scroll-area',
-      //   description: 'Visually or semantically separates content.',
-      // },
-      // {
-      //   title: 'Tabs',
-      //   href: '/docs/components/tabs',
-      //   description:
-      //     'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-      // },
-      // {
-      //   title: 'Tooltip',
-      //   href: '/docs/components/tooltip',
-      //   description:
-      //     'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-      // },
-    ],
-  },
-  {
-    title: 'Themes',
-    href: '/themes',
-  },
-  {
-    title: 'Examples',
-    href: '/examples/dashboard',
-    items: [
-      // {
-      //   title: 'Alert Dialog',
-      //   href: '/docs/components/alert-dialog',
-      //   description:
-      //     'A modal dialog that interrupts the user with important content and expects a response.',
-      // },
-      // {
-      //   title: 'Link Preview',
-      //   href: '/docs/components/link-preview',
-      //   description: 'For sighted users to preview content available behind a link.',
-      // },
-      // {
-      //   title: 'Progress',
-      //   href: '/docs/components/progress',
-      //   description:
-      //     'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-      // },
-      // {
-      //   title: 'Scroll Area',
-      //   href: '/docs/components/scroll-area',
-      //   description: 'Visually or semantically separates content.',
-      // },
-      // {
-      //   title: 'Tabs',
-      //   href: '/docs/components/tabs',
-      //   description:
-      //     'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-      // },
-      // {
-      //   title: 'Tooltip',
-      //   href: '/docs/components/tooltip',
-      //   description:
-      //     'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-      // },
-    ],
   },
   {
     title: 'Blocks',
     href: '/blocks',
   },
   {
+    title: 'Charts',
+    href: '/charts',
+  },
+  {
+    title: 'Themes',
+    href: '/themes',
+  },
+  {
     title: 'Colors',
     href: '/colors',
   },
-  // {
-  //   title: 'GitHub',
-  //   href: 'https://github.com/huntabyte/shadcn-svelte',
-  //   external: true,
-  // },
 ]
 
 export const sidebar: NavItem[] = [
